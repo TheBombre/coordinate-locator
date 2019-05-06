43a2eaa1d9b7786bd0662ad04a86bd5120c3f268
Carlton Ndhlovu <carltonndhlovu42@gmail.com>
2019-05-06 13:53:29 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,58 @@
+from random import randint
+
+computer_position_row = randint(1,5)
+computer_position_col = randint(1,5)
+
+def createGrid(col_num, row_num):
+  grid = []
+  for i in range(row_num):
+    temp_row = []
+    for i in range(col_num):
+      temp_row.append('O')
+    grid.append(temp_row)
+  return grid
+
+default_grid = createGrid(5,5)
+      
+guesses = int((len(default_grid)*len(default_grid[0]))/2)
+
+def x_marker():
+    default_grid[computer_position_row - 1][computer_position_col - 1] = 'X'
+
+def show_grid():
+  for i in range(len(default_grid)):
+    print(' '.join(default_grid[i]))
+  print('row: ', computer_position_row)  
+  print('col: ', computer_position_col)
+
+x_marker()
+
+row_num = len(default_grid)
+col_num = len(default_grid[0])
+
+while guesses > 0:
+  print('Games left: ', guesses)
+  
+  row = int(input('Guess the row from 1 to ' + str(row_num) + ':'))
+  col = int(input('Guess the col from 1 to ' + str(col_num) + ':'))
+  
+  if (row > row_num or col > col_num):
+    guesses -= 1
+    print('Those coordinates are not on the grid')
+    print('\n')
+    continue
+  
+  elif (row == computer_position_row) and (col == computer_position_col):
+    show_grid()
+    print('You Won!!')  
+    break;
+    
+  else:
+    guesses -= 1
+    print('Sorry you didn\'t find the ship. Try Again')
+    print('\n')
+    continue
+
+else: 
+  print('Game Over')
+  show_grid()
